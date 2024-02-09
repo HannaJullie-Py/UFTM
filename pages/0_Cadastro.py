80aaff278f112480e2c10c781464477b80a7302b
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-02-09 15:06:09 -0300

    Update 0_Cadastro.py

--- pages/0_Cadastro.py
+++ pages/0_Cadastro.py
@@ -3,6 +3,22 @@ import mysql.connector
 import pandas as pd
 import numpy as np
 
+def conn_mysql():
+        config = {
+            "user" : "root",
+            "password" : "ctti",
+            "host" : "127.0.0.1",
+            "port" : "3306",
+            "database" : "facul",
+            "raise_on_warnings":True
+        }
+    
+        try:
+            conexao = mysql.connector.connect(**config)
+            return conexao
+        except mysql.connector.Error as err:
+            st.error(f"Erro ao conectar ao banco MySQL: {err}")
+            return None
 
 
 st.header('Cadastro', divider='rainbow')
