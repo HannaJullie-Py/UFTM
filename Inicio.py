2a1f0efa3f6bc76908724aff3c59ad2c4fcdb170
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-02-09 14:52:15 -0300

    Update and rename conexaoBanco.py to Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -36,33 +36,14 @@ def mae():
     ##### PAGINAS #####
      
     #### Pagina 1 ####
-    def pagina_inicio():
-        st.image('https://i.imgur.com/TBvRRfw.jpg', width=720)
-        st.divider()
-        st.title('Bem-Vindo(a) à Universidade Federal de Tangamandápio (UFTM)')
+
+st.image('https://i.imgur.com/TBvRRfw.jpg', width=720)
+st.divider()
+st.title('Bem-Vindo(a) à Universidade Federal de Tangamandápio (UFTM)')
     
     
     #### Pagina 2 ####
-    
-    def pagina_cadastro():
-        st.header('Cadastro', divider='rainbow')
-        st.title('Nesta Página')
-        st.markdown('''Você vai cadastrar o aluno na Universidade Estadual de Tangamandápio (UFTM)''')
-        st.divider()
-        conexao = conn_mysql()
-        cpf = st.text_input('Digite seu CPF')
-        nome = st.text_input('Digite seu Nome')
-        curso = st.text_input('Digite o curso em que está matriculado')
-        ctt = st.text_input('Digite seu telefone de contato')
-        email = st.text_input('Digite seu Email')
-        if conexao:
-            cursor = conexao.cursor()
-            joja = st.button('Cadastrar')
-            if joja:
-                st.success("Aluno matriculado com sucesso")
-                cursor.execute('INSERT INTO alunos (CPF, Nome, Curso, Telefone, Email) values (%s,%s,%s,%s,%s)', (cpf,nome,curso,ctt,email))
-            conexao.commit()
-    
+
     #### Pagina 3 ####
     
     def pagina_bonita():
